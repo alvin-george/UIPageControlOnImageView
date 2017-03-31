3889a7b0ef78244191b4098a01d6c0ae9cee4e1d
pushpamgroupmobile <team.it@pushpamgroup.in>
2017-03-31 12:20:37 +0530

    initial commit

--- SamplePageController/ViewController.swift
+++ SamplePageController/ViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import Foundation
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
+    
+    var imageArray:[String] = []
+    
+    @IBOutlet var sampleImageView: UIImageView!
+    @IBOutlet var samplePageControl: UIPageControl!
+    
+    @IBOutlet var swipeRight: UISwipeGestureRecognizer!
+    @IBOutlet var swipeLeft: UISwipeGestureRecognizer!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imageArray = ["launch","home","recipe","burgur"]
     }
-
+    // here are those protocol methods with Swift syntax
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        return true
+    }
+    @IBAction func swipedToLeft(_ sender: Any) {
+        print("swipeLeft")
+        samplePageControl.currentPage += 1
+        
+        UIView.transition(with: sampleImageView, duration: 0.5, options: .transitionCrossDissolve, animations: {
+            self.sampleImageView.image = UIImage(named: self.imageArray[self.samplePageControl.currentPage])
+        }, completion: nil)
+        
+        
+    }
+    @IBAction func swipedToRight(_ sender: Any) {
+        print("swipeRight")
+        samplePageControl.currentPage -= 1
+        
+        UIView.transition(with: sampleImageView, duration: 0.5, options: .transitionCrossDissolve, animations: {
+            self.sampleImageView.image = UIImage(named: self.imageArray[self.samplePageControl.currentPage])
+        }, completion: nil)
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
+
+
 
